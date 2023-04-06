b4a8505bd3983732b11a2ba505a70ef819778077
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-06 09:51:19 -0700

    inconsequential changes, typo fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,19 +18,19 @@ function formSubmission(event){
     //Get project name
     let projectName = $('input[name="id-of-project-input"').val();
     if(!projectName) {
-        console.log("PLease put in a name next time :^)");
+        window.alert("Please put in a name next time :^)");
         return;
     }
     //Get project Type
     let projectType = $('input[name="id-of-project-type"]').val();
     //Get project date
     if(!projectType) {
-        console.log("PLease put in a type next time :^)");
+        window.alert("Please put in a type next time :^)");
         return;
     }
     let projectDate = $('input[name="dueDate"]').val();
     if(!projectDate) {
-        console.log("PLease put in a date next time :^)");
+        window.alert("Please put in a date next time :^)");
         return;
     }
     // .appendChild(projectName)
